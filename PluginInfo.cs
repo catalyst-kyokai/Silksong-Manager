21cad2c5526794a8cca1d979433d0e859d9079bc
Catalyst <catalyst@kyokai.ru>
2025-12-25 00:38:15 +0300

    release: v1.0.0.1PA - Speed Control & Noclip Overhaul

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -14,7 +14,7 @@ namespace SilksongManager
         public const string NAME = "Silksong Manager";
 
         /// <summary>Current plugin version (semantic versioning).</summary>
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.0.1PA";
 
         /// <summary>Plugin author name.</summary>
         public const string AUTHOR = "Catalyst";
